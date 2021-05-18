c06ae7b7511c7e86d1a7703e4c8dd494fd53621e
ljc-614 <ljc_computer@sina.com>
2021-05-18 10:02:07 +0800

    111

--- MyTest/Program.cs
+++ MyTest/Program.cs
@@ -11,7 +11,6 @@ namespace MyTest
         static void Main(string[] args)
         {
             Console.Write("123");
-            Console.Write("101010");
             Console.ReadKey();
         }
     }
